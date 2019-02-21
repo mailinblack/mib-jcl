4ba37011cf6c332b8aa9ecd9f8e1ab22ec0fe1c6
dregnier <dregnier@mailinblack.com>
2019-02-21 17:10:39 +0100

    Add MibBooleans.and()

---
+++ mib-jcl-common/src/main/java/com/mib/primitives/MibBooleans.java
@@ -0,0 +1,33 @@
+package com.mib.primitives;
+
+/**
+ * Utility methods for booleans.
+ */
+public enum MibBooleans {
+    ;
+
+    /**
+     * Returns {@code true} if all given boolean values are {@code true}.
+     * <p>
+     * Similar to the equivalent of Apache Commons Lang3 {@code BooleanUtils.and()}
+     * but does not have this <a href="https://stackoverflow.com/questions/49252638/how-to-use-apache-commons-booleanutils-and-method">issue</a>.
+     *
+     * @param firstOne the first boolean value
+     * @param others the other booleans as a vararg
+     * @return {@code true} if all given boolean values are {@code true}
+     */
+    public static boolean and(boolean firstOne, boolean... others) {
+        if(!firstOne) {
+            return false;
+        }
+        if(others == null || others.length == 0) {
+            return true;
+        }
+        for (final boolean element : others) {
+            if (!element) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
