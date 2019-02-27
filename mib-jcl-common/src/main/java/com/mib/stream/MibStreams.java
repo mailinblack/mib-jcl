340009e66729916d7c4fd91725e369e39efa5b7d
dregnier <dregnier@mailinblack.com>
2019-02-27 17:25:27 +0100

    Add MibStreams.repeatTwice()

--- mib-jcl-common/src/main/java/com/mib/stream/MibStreams.java
+++ mib-jcl-common/src/main/java/com/mib/stream/MibStreams.java
@@ -4,12 +4,14 @@ import static java.util.Spliterators.spliteratorUnknownSize;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
 import static java.util.stream.StreamSupport.stream;
 
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
@@ -47,5 +49,16 @@ public enum MibStreams {
      */
     public static <T, C> Stream<List<T>> groupBy(final Stream<T> stream, final Function<T, C> groupingFunction) {
         return stream.collect(groupingBy(groupingFunction, mapping(u -> u, toList()))).values().stream();
+    }
+
+    /**
+     * Shortcut to repeat two times a stream based on the given {@link Stream} supplier.
+     *
+     * @param streamSupplier the stream {@link Supplier}
+     * @param <T> the stream element type
+     * @return concatenated stream.
+     */
+    public static <T> Stream<T> repeatTwice(final Supplier<Stream<T>> streamSupplier) {
+        return concat(streamSupplier.get(), streamSupplier.get());
     }
 }
--- mib-jcl-common/src/test/java/com/mib/stream/MibStreamsTest.java
+++ mib-jcl-common/src/test/java/com/mib/stream/MibStreamsTest.java
@@ -1,11 +1,14 @@
 package com.mib.stream;
 
+import static com.google.common.collect.ImmutableList.of;
+import static com.mib.stream.MibStreams.repeatTwice;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +20,7 @@ class MibStreamsTest {
     @Test
     void shouldConvertToStream() {
 
-        final Iterator<String> iterator = ImmutableList.of("e1", "e2", "e3").iterator();
+        final Iterator<String> iterator = of("e1", "e2", "e3").iterator();
 
         final Stream<String> stream = MibStreams.toStream(iterator);
 
@@ -46,6 +49,15 @@ class MibStreamsTest {
         groupsOfCities.forEach(group -> assertThat(group.size()).isEqualTo(2));
     }
 
+    @Test
+    void shouldRepeatTwiceCorrectly() {
+        final Supplier<Stream<String>> supplier = () -> of("e1", "e2").stream();
+
+        final List<String> repeated = repeatTwice(supplier).collect(toList());
+
+        assertThat(repeated).containsExactly("e1", "e2", "e1", "e2");
+    }
+
     private static class CustomAddress {
 
         private final String cityName;
@@ -54,7 +66,7 @@ class MibStreamsTest {
             this.cityName = cityName;
         }
 
-        public String cityName() {
+        String cityName() {
             return cityName;
         }
     }
